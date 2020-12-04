c8dff264427b7a719826d1986e54920e3eccc9ab
Alex Marmolejo <alex97marmolejo@gmail.com>
2020-12-04 02:18:29 -0500

    added delete function and data attribute updates to cards

--- main.js
+++ main.js
@@ -13,51 +13,67 @@ class Book {
         this.author = author;
         this.pages = pages;
         this.isRead = isRead;
-        this.addToLibrary = function () {
-            myLibrary.push(this);
+    }
+
+    addToLibrary() {
+        myLibrary.push(this);
+    }
+
+    createNewCard() {
+        const newDiv = document.createElement('div');
+        newDiv.className = 'card';
+        newDiv.setAttribute('data-index', myLibrary.indexOf(this))
+
+        const newTitle = document.createElement('h2');
+        newTitle.className = 'title';
+        newTitle.textContent = this.title;
+
+        const newAuthor = document.createElement('p');
+        newAuthor.className = 'author';
+        newAuthor.textContent = `by: ${this.author}`;
+
+        const newPageCount = document.createElement('p');
+        newPageCount.className = 'pageCount';
+        newPageCount.textContent = `page count: ${this.pages}`;
+    
+        const newForm = document.createElement('form');
+    
+        const label = document.createElement('span');
+        label.textContent = 'Read Status:';
+    
+        const checkBox = document.createElement("input");
+        checkBox.setAttribute('type','checkbox');
+        checkBox.setAttribute('name','read');
+        if(bookRead[0].checked === true) {
+            checkBox.checked = true;
         }
-        this.createNewCard = () => {
-            const newDiv = document.createElement('div');
-            newDiv.className = 'card';
 
-            const newTitle = document.createElement('h2');
-            newTitle.className = 'title';
-            newTitle.textContent = title;
+        newForm.appendChild(label);
+        newForm.appendChild(checkBox);
 
-            const newAuthor = document.createElement('p');
-            newAuthor.className = 'author';
-            newAuthor.textContent = `By: ${author}`;
+        const newDeleteButton = document.createElement('button');
+        newDeleteButton.textContent = 'delete';
+        newDeleteButton.className = 'delete-button';
+    
+        deck.appendChild(newDiv);
+        newDiv.appendChild(newTitle);
+        newDiv.appendChild(newAuthor);
+        newDiv.appendChild(newPageCount);
+        newDiv.appendChild(newForm);
+        newDiv.appendChild(newDeleteButton)
+    }
+}
 
-            const newPageCount = document.createElement('p');
-            newPageCount.className = 'pageCount';
-            newPageCount.textContent = `Number of Pages: ${pages}`;
-        
-            const f = document.createElement('form');
-        
-            const l = document.createElement('span');
-            l.textContent = 'read status:';
-        
-            const i = document.createElement("input");
-            i.setAttribute('type','checkbox');
-            i.setAttribute('name','read');
-            if(bookRead[0].checked === true) {
-                i.checked = true;
-            }
-        
-            f.appendChild(l);
-            f.appendChild(i);
+function resetValues() {
+    bookTitle.value = '';
+    bookAuthor.value = '';
+    bookPages.value = '';
+    bookRead[1].checked = true;
+}
 
-            const deleteCardText = document.createElement('button');
-            deleteCardText.textContent = 'delete';
-            deleteCardText.className = 'delete-button';
-        
-            deck.appendChild(newDiv);
-            newDiv.appendChild(newTitle);
-            newDiv.appendChild(newAuthor);
-            newDiv.appendChild(newPageCount);
-            newDiv.appendChild(f);
-            newDiv.appendChild(deleteCardText)
-        }
+function updateIndex() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        deck.children[i].dataset.index = i;
     }
 }
 
@@ -66,12 +82,19 @@ submitButton.addEventListener('click', () => {
 
     book.addToLibrary();
     book.createNewCard();
-
-    bookTitle.value = '';
-    bookAuthor.value = '';
-    bookPages.value = '';
-    bookRead[1].checked = true;
+    updateIndex();
+    resetValues();
 
     console.log(myLibrary);
-    console.log(card);
+})
+
+deck.addEventListener('click', (e) => {
+    let data = e.target.parentNode.dataset.index;
+
+    if (e.target.className === 'delete-button') {
+        e.target.parentNode.remove();
+        myLibrary.splice(data, 1)   
+        updateIndex();
+        console.log(myLibrary);
+    }
 })
